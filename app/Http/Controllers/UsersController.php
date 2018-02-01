3a67e2769fda9fd4bb57b3a5619832394691481c
刘毅 <liuyi61445@gmail.com>
2018-02-01 14:30:12 +0800

    用户头像上传

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
-
+use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
     /**
@@ -27,9 +27,19 @@ class UsersController extends Controller
     /**
      * 修改用户信息
      */
-    public function update(UserRequest $request, User $user){
+    public function update(UserRequest $request,ImageUploadHandler $uploader, User $user){
 
-        $user->update($request->all());
+        $data = $request->all();
+        //处理上传的文件
+        if($request->avatar){
+            $result = $uploader->save($request->avatar,'avatars',$user->id,362);
+            if($result){
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        //更新
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
 
     }
