716dda4783767627e2190d9cdc97bc5dc1ff9193
刘毅 <liuyi61445@gmail.com>
2018-02-01 18:40:40 +0800

    删除帖子

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -10,20 +10,11 @@ class UserPolicy
     use HandlesAuthorization;
 
     /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * 在用户修改的时候验证
      */
     public function update(User $currentUser, User $user){
         //第一个参数代表登录的user，第二个代表需要修改的user
         return $currentUser->id === $user->id;
     }
+
 }
