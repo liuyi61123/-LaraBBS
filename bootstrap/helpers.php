75907363f2749aeb13deff0df59466773ea9fe26
刘毅 <liuyi61445@gmail.com>
2018-02-01 17:26:48 +0800

    新建帖子

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,3 +3,12 @@
 function route_class(){
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+/**
+ * 截取文章摘要
+ */
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
