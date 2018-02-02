bba916326c35d56eb4f01eec2a6510c0ae4a3127
刘毅 <liuyi61445@gmail.com>
2018-02-02 17:31:59 +0800

    侧边栏活跃用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasRoles;
+    use HasRoles,Traits\ActiveUserHelper;
     use Notifiable{
          notify as protected laravelNotify;
     }
