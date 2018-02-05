5aeee1f935d6c141edd9ff87efdc753a99a3116e
liuyi <475793342@qq.com>
2018-02-05 18:08:39 +0800

    添加图片验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use Cache;
 use Illuminate\Http\Request;
 use Overtrue\EasySms\EasySms;
 use App\Http\Requests\Api\VerificationCodeRequest;
@@ -9,7 +10,19 @@ use App\Http\Requests\Api\VerificationCodeRequest;
 class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request,EasySms $easySms){
-        $phone = $request->phone;
+
+        $captchaData = Cache::get($request->captcha_key);
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+           // 验证错误就清除缓存
+           Cache::forget($request->captcha_key);
+           return $this->response->errorUnauthorized('验证码错误');
+       }
+
+       $phone = $captchaData['phone'];
 
         if (!app()->environment('production')) {
             //测试环境下
@@ -36,7 +49,10 @@ class VerificationCodesController extends Controller
         $key = 'verificationCode_'.str_random(15);
         $expiredAt = now()->addMinutes(5);
         // 缓存验证码 5分钟过期。
-        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        // 清除图片验证码缓存
+        Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
